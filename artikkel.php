ae71b8af22f1a58bee6ef06d00fbb71558c0fe2d
fredm <fred.github@moerman.no>
2016-11-02 20:15:17 +0100

    alt php

---
+++ artikkel.php
@@ -0,0 +1,51 @@
+<?php 
+$bodynavn = "artikkel";
+
+include("hodet.ink.php");
+?>
+
+	<div class="wrapper clearfix">
+		<article class="dobbel"><div class="littrom">
+			<header>
+
+	  	dette er AAA
+	  </header>
+	  <div class="dato">fredm - 20160823</div>
+			<p>
+				<figure class="hh"><img src="./pics/mini01.jpg"></figure> 
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. <br>
+				Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>
+				Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				<figure class="vv"><img src="./pics/mini02.jpg"></figure> 
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>
+				Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>
+				Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br>
+			</p>
+			<footer>
+				....
+			</footer>
+		</div></article>
+
+		<aside>
+			<p>kdjsh lkjh lhg dsf</p>
+			<p>ghng trfhtytn rtyh ety ty </p>
+			<p>drig ubh oibvgu ytd gbv f</p>
+			<p>pkjn ojin ytfc 6rx u</p>
+		</aside>
+	</div>
+
+
+
+<?php 
+include("fotter.ink.php");
+?>
