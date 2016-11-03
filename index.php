fd3bbfaf074fa62c2c90f8cc76b1112954f4aee1
fredm-fraJobbPC <fred.moerman@nb.no>
2016-11-03 08:06:54 +0100

    fjernet noen tegn

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ include("hodet.ink.php");
 	  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 	  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 	  consequat. </p>
-	  ><br>
+	  <br>
 	  <a href="artikkel.php">les mer...</a>
   </div></article>
 
